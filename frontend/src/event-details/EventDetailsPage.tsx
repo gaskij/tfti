eed8dc1e2c48b8127cf1c42fb7f6341370cf23c3
Justin Gaskins <gaskij@rpi.edu>
2020-11-30 16:43:15 -0500

    Begin event details page

---
+++ frontend/src/event-details/EventDetailsPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Grid, Box, Typography } from '@material-ui/core';
+
+const EventDetailsPage = () => {
+
+  return (
+    <div></div>
+  )
+}
+
+export default EventDetailsPage;
